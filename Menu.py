7aca061dd22468e73a3c3fb9debd521fcc3067c0
dar-dreas <andreas.elias.jakob.meuwissen@vub.be>
2026-03-23 16:56:51 +0100

    andrei bg and menu imrovements

---
+++ Menu.py
@@ -0,0 +1,37 @@
+import pygame
+from config import HEIGHT, WIDTH, SCREEN
+
+class Button:
+    def __init__(self, y_pos, width, height, text, text_size, text_color, text_hov_color, button_color, button_h_color, GAME, state_change, mouse_vis):
+        self.y_pos = y_pos
+        self.h = height
+        self.w = width
+        self.text = text
+        self.text_size = text_size
+        self.text_color = text_color
+        self.text_hov_color = text_hov_color
+        self.button_color = button_color
+        self.button_hov_color = button_h_color
+        self.GAME = GAME
+        self.state_change = state_change
+        self.mouse_vis = mouse_vis
+    def draw_button(self, events):
+        mouse = pygame.mouse.get_pos()
+        hovering = (WIDTH/2-self.w/2 <= mouse[0] <= WIDTH/2+self.w/2 and HEIGHT/2-self.h/2+self.y_pos <= mouse[1] <= HEIGHT/2+self.h/2+self.y_pos)
+        for ev in events:
+            if ev.type == pygame.MOUSEBUTTONDOWN and ev.button == 1:
+                if hovering:
+                    pygame.mouse.set_visible(self.mouse_vis)
+                    pygame.event.set_grab(True)
+                    self.GAME.state = self.state_change
+                    if self.state_change == "menu":
+                        self.GAME.reset_game()
+                    if self.state_change == "Stop":
+                        self.GAME.running = False
+                        self.GAME.state = None
+                
+        color = self.button_hov_color if hovering else self.button_color
+        text_color = self.text_hov_color if hovering else self.text_color
+        
+        pygame.draw.rect(SCREEN,color,[WIDTH/2-self.w/2,HEIGHT/2-self.h/2+self.y_pos,self.w,self.h])
+        SCREEN.blit(pygame.font.SysFont('Corbel', self.text_size, True).render(self.text, True, text_color),(WIDTH/2-self.w/4,HEIGHT/2-self.h/4+self.y_pos))
