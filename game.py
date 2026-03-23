2a5be30012864f625237a459b8a79db8868291b0
dar-dreas <andreas.elias.jakob.meuwissen@vub.be>
2026-03-23 12:27:45 +0100

    fixed reset

--- game.py
+++ game.py
@@ -193,10 +193,13 @@ class Game:
                 for ev in pygame.event.get():
                     if ev.type == pygame.MOUSEBUTTONDOWN:
                         if (WIDTH/2-qw/2 <= mouse[0] <= WIDTH/2+qw/2 and HEIGHT/2-qh/2+HEIGHT/4 <= mouse[1] <= HEIGHT/2+qh/2+HEIGHT/4):
+                            self.player = Player(150, 150)
+                            self.pistol = Pistol()
+                            self.current_gun = self.pistol
+                            self.enemies = self.create_enemies()
                             self.game_running = False
-                            self.menu_running = False
+                            self.menu_running = True
                             self.paused = False
-                            self.run()
                         if (WIDTH/2-sw/2 <= mouse[0] <= WIDTH/2+sw/2 and HEIGHT/2-sh/2 <= mouse[1] <= HEIGHT/2+sh/2):
                             pygame.mouse.set_visible(False)
                             pygame.event.set_grab(True)
